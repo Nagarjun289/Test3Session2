55e0a2b6400dba6fd53739eb434745fd59ea2a8f
nagarjun nataraj <94677992+Nagarjun289@users.noreply.github.com>
2022-03-25 10:10:19 +0000

    b

--- p1final.c
+++ p1final.c
@@ -1,1 +1,30 @@
 #include <stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  printf("enter the frist numerator: \n" );
+  scanf("%d" num1);
+  printf("enter the frist denomenator: \n");
+  scanf("%d" den1);
+  printf("enter the second numerator: \n");
+  scanf("%d" num2);
+  printf(" enter the second denomenator" \n");
+  scanf("%d" den2);
+}
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3, sum)
+{
+  printf()
+}
+void output(int num1, int den1, int num2, int den2, int num3, int den3);
+{
+ printf("sum of %d/%d and %d/%d is %d/%d\n", ) 
+}
+int main()
+{
+  int num1,den1,num2,den2,sum;
+  input(&num1,&den1,&num2,&den2);
+  add(num1,den1,num2,den2,sum);
+  output(a,b,sum);
+}
+
+ 
+  
